90195d4bffe924157aeedc4d67035d5a3d1203dc
chenshengshui <1382441007.com>
2020-04-12 22:02:36 +0800

    modify: 统一路由变量

--- app/router.ts
+++ app/router.ts
@@ -1,13 +1,13 @@
 import { Application } from 'egg';
 
-import artistRouter from './router/artist';
+import ArtistRouter from './router/artist';
 import AlbumRouter from './router/artist';
 import LoginRouter from './router/login';
 import UserRouter from './router/user';
 
 export default (app: Application) => {
   LoginRouter(app);
-  artistRouter(app);
+  ArtistRouter(app);
   AlbumRouter(app);
   UserRouter(app);
 };
